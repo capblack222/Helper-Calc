153b03738c16a787f09d43be77d6ddfee3688c28
Nishtha Gupta <66949977+capblack222@users.noreply.github.com>
2021-06-28 23:14:34 +0530

    initial JS file

---
+++ calc.js
@@ -0,0 +1,17 @@
+// Functions to get and print the value for history element
+function getHistory() {
+    return document.getElementById("h_value").innerText;
+}
+function printHistory(num) {
+    document.getElementById("h_value").innerText = num;
+}
+
+// functions to get and print the output element values
+function getOutput() {
+    return document.getElementById("out_value").innerText;
+}
+function printOutput(num) {
+    document.getElementById("out_value").innerText = num;
+}
+
+
